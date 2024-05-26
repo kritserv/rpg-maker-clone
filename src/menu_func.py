9fe5d10aeab5b67bf795aebd9feb99a5d93baa37
kritserv <krit.serv@gmail.com>
2024-05-26 20:06:48 +0700

    use exec to execute menu func

--- src/menu_func.py
+++ src/menu_func.py
@@ -7,38 +7,37 @@ from .output_file import write_line_to_file, read_line_from_file
 @dataclass(slots=True)
 class MenuFunc:
 	current_project_name: str = ""
-	func: dict[str, int] = field(
+	func: dict[str, str] = field(
 		default_factory=lambda: ({
-			"New project (Ctrl + N)": 0, "new_project": 0,
-			"Open project (Ctrl + O)": 1, "open_project": 1,
-			"Close project": 2,
-			"Save Project (Ctrl + S)": 3, "save_project": 3,
-			"Exit (Ctrl + Q)": 4,
-			"Undo (Ctrl + Z)": 5, "undo": 5,
-			"Cut (Ctrl + Z)": 6, "cut": 6,
-			"Copy (Ctrl + C)": 7, "copy": 7,
-			"Paste (Ctrl + V)": 8, "paste": 8,
-			"Delete (Del)": 9, "delete": 9,
-			"Current and Below (F2)": 10,
-			"All Layers (F3)": 11,
-			"Dim Other Layers (F4)": 12,
-			"Layer 1 (F5)": 13, "layer_1": 13,
-			"Layer 2 (F6)": 14, "layer_2": 14,
-			"Layer 3 (F7)": 15, "layer_3": 15,
-			"Events (F8)": 16, "events": 16,
-			"Pencil": 17, "pencil": 17,
-			"Rectangle": 18, "rectangle": 18,
-			"Ellipse": 19, "ellipse": 19,
-			"Flood Fill": 20, "flood_fill": 20,
-			"Select": 21, "select": 21,
-			"1:1": 22, "1to1": 22,
-			"1:2": 23, "1to2": 23,
-			"1:4": 24, "1to4": 24,
-			"Database (F9)": 25, "database": 25,
-			"Options": 26,
-			"Play Test (F12)": 27, "play_test": 27,
-			"Change Title": 28,
-			"Open Game Folder": 29
+			"New project (Ctrl + N)": "self.new_project(terminal)", "new_project": "self.new_project(terminal)",
+			"Open project (Ctrl + O)": "self.open_project(terminal)", "open_project": "self.open_project(terminal)",
+			"Close project": "self.close_project()",
+			"Save Project (Ctrl + S)": "pass", "save_project": "pass",
+			"Undo (Ctrl + Z)": "pass", "undo": "pass",
+			"Cut (Ctrl + X)": "pass", "cut": "pass",
+			"Copy (Ctrl + C)": "pass", "copy": "pass",
+			"Paste (Ctrl + V)": "pass", "paste": "pass",
+			"Delete (Del)": "pass", "delete": "pass",
+			"Current and Below (F2)": "self.change_current_mode((('current_and_below', True), ('all_layer', False)))",
+			"All Layers (F3)": "self.change_current_mode((('current_and_below', False), ('all_layer', True)))",
+			"Dim Other Layers (F4)": "self.change_current_mode((('dim_others_layer', 'toggle')))",
+			"Layer 1 (F5)": "self.current_layer = 1", "layer_1": "self.current_layer = 1",
+			"Layer 2 (F6)": "self.current_layer = 2", "layer_2": "self.current_layer = 2",
+			"Layer 3 (F7)": "self.current_layer = 3", "layer_3": "self.current_layer = 3",
+			"Events (F8)": "pass", "events": "pass",
+			"Pencil": "self.current_tool = 'pencil'", "pencil": "self.current_tool = 'pencil'",
+			"Rectangle": "self.current_tool = 'rectangle'", "rectangle": "self.current_tool = 'rectangle'",
+			"Ellipse": "self.current_tool = 'ellipse'", "ellipse": "self.current_tool = 'ellipse'",
+			"Flood Fill": "self.current_tool = 'flood_fill'", "flood_fill": "self.current_tool = 'flood_fill'",
+			"Select": "self.current_tool = 'select'", "select": "self.current_tool = 'select'",
+			"1:1": "self.zoom_level = 1.0", "1to1": "self.zoom_level = 1.0",
+			"1:2": "self.zoom_level = 0.5", "1to2": "self.zoom_level = 0.5",
+			"1:4": "self.zoom_level = 0.25", "1to4": "self.zoom_level = 0.25",
+			"Database (F9)": "pass", "database": "pass",
+			"Options": "pass",
+			"Play Test (F12)": "self.play_test(terminal)", "play_test": "self.play_test(terminal)",
+			"Change Title": "self.change_title(terminal)",
+			"Open Game Folder": "self.open_game_folder(terminal)"
 		})
 		)
 	current_tool: str = "pencil"
@@ -51,74 +50,6 @@ class MenuFunc:
 		)
 	current_layer: int = 1
 	zoom_level: float = 1.00
-
-	def exec_func(self, func_id, terminal) -> int or None:
-		if func_id == 0:
-			self.new_project(terminal)
-		elif func_id == 1:
-			self.open_project(terminal)
-		elif func_id == 2:
-			self.close_project()
-		elif func_id == 3: #save project
-			pass
-		elif func_id == 4:
-			return 0
-		elif func_id == 5: #undo
-			pass
-		elif func_id == 6: #cut
-			pass
-		elif func_id == 7: #copy
-			pass
-		elif func_id == 8: #paste
-			pass
-		elif func_id == 9: #delete
-			pass
-		elif func_id == 10:
-			self.current_mode["current_and_below"] = True
-			self.current_mode["all_layer"] = False
-		elif func_id == 11:
-			self.current_mode["current_and_below"] = False
-			self.current_mode["all_layer"] = True
-		elif func_id == 12:
-			if self.current_mode["dim_others_layer"] == True:
-				self.current_mode["dim_others_layer"] = False
-			else:
-				self.current_mode["dim_others_layer"] = True
-		elif func_id == 13:
-			self.current_layer = 1
-		elif func_id == 14:
-			self.current_layer = 2
-		elif func_id == 15:
-			self.current_layer = 3
-		elif func_id == 16: #event
-			pass
-		elif func_id == 17:
-			self.current_tool = "pencil"
-		elif func_id == 18:
-			self.current_tool = "rectangle"
-		elif func_id == 19:
-			self.current_tool = "eclipse"
-		elif func_id == 20:
-			self.current_tool = "flood_fill"
-		elif func_id == 21:
-			self.current_tool = "select"
-		elif func_id == 22:
-			self.zoom_level = 1.0
-		elif func_id == 23:
-			self.zoom_level = 0.5
-		elif func_id == 24:
-			self.zoom_level = 0.25
-		elif func_id == 25: #database
-			pass
-		elif func_id == 26: #options
-			pass
-		elif func_id == 27:
-			self.play_test(terminal)
-		elif func_id == 28:
-			self.change_title(terminal)
-		elif func_id == 29:
-			self.open_game_folder(terminal)
-		return None
 
 	def mkdir(self, dir_name) -> None:
 		makedirs(dir_name, exist_ok=True)
@@ -171,6 +102,13 @@ class MenuFunc:
 	def close_project(self) -> None:
 		self.current_project_name = ""
 
+	def change_current_mode(self, changes) -> None:
+		for mode_name, change in changes:
+			if change == "toggle":
+				self.current_mode[mode_name] = not self.current_mode[mode_name]
+			else:
+				self.current_mode[mode_name] = change
+
 	def play_test(self, terminal) -> None:
 		terminal.command(f"cd projects/{self.current_project_name} && python main.py")
 
@@ -180,17 +118,22 @@ class MenuFunc:
 	def open_game_folder(self, terminal) -> None:
 		terminal.open_project_folder(self.current_project_name)
 
-	def get_func_id(self, clicked_menu) -> int:
+	def get_func(self, clicked_menu) -> int:
 		return self.func[clicked_menu]
 
 	def update(self, clicked_menu, terminal) -> int or None:
 		return_value = None
 		if clicked_menu:
-			func_id = self.get_func_id(clicked_menu)
-			need_project = func_id not in [0, 1, 4]
+			if clicked_menu == "Exit (Ctrl + Q)":
+				return 0
+			func = self.get_func(clicked_menu)
+			need_project = func not in set((
+				"self.new_project(terminal)", 
+				"self.open_project(terminal)"
+				))
 			if need_project:
 				if self.project_has_opened():
-					return_value = self.exec_func(func_id, terminal)
+					exec(func)
 			else:
-				return_value = self.exec_func(func_id, terminal)
+				exec(func)
 		return return_value
