4e152c21ed95754d5e7cb683516e5c7d05a31597
kritserv <krit.serv@gmail.com>
2024-10-06 15:40:51 +0700

    add css

--- src/load_json.py
+++
@@ -1,7 +0,0 @@
-from json import load
-
-def json_loader(path) -> dict:
-	with open(path) as f:
-		json_load = load(f)
-		f.close()
-	return json_load
--- src/sub_program/src/dump_json.py
+++
@@ -1,6 +0,0 @@
-from json import dump
-
-def json_dumper(path, content) -> None:
-	with open(path, "w") as f:
-		dump(content, f)
-		f.close()
