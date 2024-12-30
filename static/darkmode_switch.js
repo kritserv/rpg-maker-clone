133a1fd96d696014902ae6642af566c247e3f73f
kritserv <krit.serv@gmail.com>
2024-12-30 21:30:25 +0700

    add dark mode switch

--- static/darkmode_switch.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("darkSwitch").addEventListener("click", () => {
-  if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
-    document.documentElement.setAttribute("data-bs-theme", "light");
-  } else {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
-});
